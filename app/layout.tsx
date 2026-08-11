c986a75ddb5c88e20dc3d6c5d3e5af9ec2324620
zoey <zoeyzbeezbee@gmail.com>
2026-08-11 06:23:23 +0500

    Load motion rebuild visual layer last

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./clarity.css";
 import "./motion-v2.css";
 import "./motion-hotfix.css";
 import "./recover-inspired.css";
+import "./motion-rebuild.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
